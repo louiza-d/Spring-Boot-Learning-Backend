6c384b99b7adbba71cef9da01f9616ac9bf0d2b7
unknown <louizadjoudad@gmail.com>
2025-11-02 11:20:19 +0100

    feat : add @CrossOrigin('*') for all origins

--- src/main/java/com/codewithProject/employee/controller/EmployeeController.java
+++ src/main/java/com/codewithProject/employee/controller/EmployeeController.java
@@ -3,15 +3,13 @@ package com.codewithProject.employee.controller;
 import com.codewithProject.employee.entity.Employee;
 import com.codewithProject.employee.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
