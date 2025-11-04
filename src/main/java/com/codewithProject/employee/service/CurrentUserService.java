3011947efee00fddcae8082e3f36acd9db20370f
unknown <louizadjoudad@gmail.com>
2025-11-04 11:16:13 +0100

    feat: Add current user service

---
+++ src/main/java/com/codewithProject/employee/service/CurrentUserService.java
@@ -0,0 +1,24 @@
+package com.codewithProject.employee.service;
+
+import com.codewithProject.employee.entity.User;
+import com.codewithProject.employee.repository.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CurrentUserService {
+
+    private final UserRepository userRepository;
+
+    public CurrentUserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Current user not found"));
+    }
+}
