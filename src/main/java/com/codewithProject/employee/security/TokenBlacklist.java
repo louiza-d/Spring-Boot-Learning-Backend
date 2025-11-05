def4dae5adc1ce40cf6bf48f12fe956f2d3a69ff
unknown <louizadjoudad@gmail.com>
2025-11-05 15:36:08 +0100

    feat : add classe for the invalid tokens

--- src/main/java/com/codewithProject/employee/security/TokenBlacklist.java
+++ src/main/java/com/codewithProject/employee/security/TokenBlacklist.java
@@ -1,4 +1,21 @@
 package com.codewithProject.employee.security;
 
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Component
 public class TokenBlacklist {
+
+    private final Set<String> blacklist = new HashSet<>();
+
+    public void add (String token) {
+        blacklist.add(token);
+    }
+
+    public boolean contains (String token) {
+        return  blacklist.contains(token);
+    }
 }
