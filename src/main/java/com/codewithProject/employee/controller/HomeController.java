7328ce7319f461c5f9f3dea42881ea8d8c40d85b
unknown <louizadjoudad@gmail.com>
2025-11-06 12:17:07 +0100

    feat: add server-side controller that manages the HTML page

--- src/main/java/com/codewithProject/employee/controller/HomeController.java
+++ src/main/java/com/codewithProject/employee/controller/HomeController.java
@@ -1,4 +1,16 @@
 package com.codewithProject.employee.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Welcome to our Employee Management System ");
+        model.addAttribute("message", "Empower your team and streamline workforce management with our intuitive Employee Management System");
+        return "index";
+    }
 }
