6d7bbc072c6eef55f2c9939b79bc037bf4f94cee
unknown <louizadjoudad@gmail.com>
2025-11-09 15:25:07 +0100

    add Error Response DTO

--- src/main/java/com/codewithProject/employee/response/ErrorResponse.java
+++ src/main/java/com/codewithProject/employee/response/ErrorResponse.java
@@ -1,4 +1,30 @@
 package com.codewithProject.employee.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Schema(description = "Réponse d'erreur")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ErrorResponse {
+
+    @Schema(description = "Code d'erreur HTTP", example = "404")
+    private int status;
+
+    @Schema(description = "Message d'erreur", example = "Employé introuvable")
+    private String message;
+
+    @Schema(description = "Timestamp", example = "2025-11-09T10:30:00")
+    private LocalDateTime timestamp;
+
+    public ErrorResponse (int status, String message ){
+        this.status = status;
+        this.message = message;
+        this.timestamp = LocalDateTime.now();
+    }
 }
