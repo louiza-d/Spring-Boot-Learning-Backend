0b8399abf946ae47832745c144c7716fc593fdd0
unknown <louizadjoudad@gmail.com>
2025-11-09 15:41:54 +0100

    refactor: add DTOs , validation &  swagger docs

--- src/main/java/com/codewithProject/employee/controller/EmployeeController.java
+++ src/main/java/com/codewithProject/employee/controller/EmployeeController.java
@@ -1,13 +1,18 @@
 package com.codewithProject.employee.controller;
 
 import com.codewithProject.employee.entity.Employee;
+import com.codewithProject.employee.request.EmployeeRequest;
+import com.codewithProject.employee.response.EmployeeResponse;
+import com.codewithProject.employee.response.ErrorResponse;
 import com.codewithProject.employee.service.EmployeeService;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -19,40 +24,50 @@ public class EmployeeController implements IEmployeeController {
 
     @Override
     @PostMapping("/employee")
-    public Employee postEmployee(@RequestBody Employee employee) {
-
-        return employeeService.postEmployee(employee);
+    public EmployeeResponse postEmployee(@RequestBody @Valid EmployeeRequest request) {
+        return employeeService.postEmployee(request);
     }
 
+    @Override
     @GetMapping("/employees")
-    public List<Employee> getAllEmployees() {
+    public List<EmployeeResponse> getAllEmployees() {
         return employeeService.getAllEmployees();
     }
 
+    @Override
     @DeleteMapping("/employee/{id}")
     public ResponseEntity<?> deleteEmployee(@PathVariable long id) {
         try {
             employeeService.deleteEmployee(id);
-            return new ResponseEntity<>("Employee with ID" + id + "deleted successfully", HttpStatus.OK);
+            return ResponseEntity.ok("Employee with ID " + id + " deleted successfully");
         } catch (EntityNotFoundException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ErrorResponse(404, e.getMessage(), LocalDateTime.now()));
         }
     }
 
+    @Override
     @GetMapping("/employee/{id}")
     public ResponseEntity<?> getEmployeeById(@PathVariable long id) {
-        Employee employee = employeeService.getEmployeeById(id);
-        if (employee == null)
-            return ResponseEntity.notFound().build();
-        return ResponseEntity.ok(employee);
+        try {
+            EmployeeResponse employee = employeeService.getEmployeeById(id);
+            return ResponseEntity.ok(employee);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ErrorResponse(404, e.getMessage(), LocalDateTime.now()));
+        }
     }
 
     @PatchMapping("/employee/{id}")
-    public ResponseEntity<?> updateEmployee(@PathVariable long id, @RequestBody Employee employee) {
-        Employee updatedEmployee = employeeService.updateEmployee(id, employee);
-
-        if (updatedEmployee == null)
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        return ResponseEntity.ok(updatedEmployee);
-    }
-}
+    public ResponseEntity<?> updateEmployee(@PathVariable long id, @RequestBody @Valid  EmployeeRequest request) {
+        try {
+            EmployeeResponse updatedEmployee = employeeService.updateEmployee(id, request);
+            return ResponseEntity.ok(updatedEmployee);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ErrorResponse(404, e.getMessage(), LocalDateTime.now()));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new ErrorResponse(400, e.getMessage(), LocalDateTime.now()));
+        }
+} }
