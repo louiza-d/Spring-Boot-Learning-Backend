b84b2f032a8475522a67c4c6c39eb3d320a77ea7
unknown <louizadjoudad@gmail.com>
2025-11-11 19:33:27 +0100

    add message DTO Response

--- src/main/java/com/codewithProject/employee/response/MessageResponse.java
+++ src/main/java/com/codewithProject/employee/response/MessageResponse.java
@@ -1,4 +1,17 @@
 package com.codewithProject.employee.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Schema(description = "Réponse avec message")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MessageResponse {
+
+
+    @Schema(description = "Message de réponse", example = "Déconnexion réussie")
+    private String message;
 }
