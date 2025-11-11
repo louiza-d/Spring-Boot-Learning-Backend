ea0c053a419930b289a6735d4972e1f26d2cade3
unknown <louizadjoudad@gmail.com>
2025-11-11 18:02:16 +0100

    add login DTO Request

--- src/main/java/com/codewithProject/employee/request/LoginRequest.java
+++ src/main/java/com/codewithProject/employee/request/LoginRequest.java
@@ -1,4 +1,24 @@
 package com.codewithProject.employee.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Schema(description = "Requete de connexion")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
+
+    @Schema(description = "Adress email", example = "user@gmail.com", requiredMode = Schema.RequiredMode.REQUIRED)
+    @NotBlank(message = "L'email est obligatoire")
+    @Email(message = "Format d'email invalide")
+    private  String email;
+
+    @Schema(description = "Mot de passe", example = "Password123!", requiredMode = Schema.RequiredMode.REQUIRED)
+    @NotBlank(message = "Le mot de passe est obligatoire")
+    private String password;
 }
