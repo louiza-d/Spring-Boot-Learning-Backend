f95677faebb5794162721a0b750f719490c9958d
unknown <louizadjoudad@gmail.com>
2025-11-13 14:47:51 +0100

    refactor: add EmailService

--- src/main/java/com/codewithProject/employee/service/AuthService.java
+++ src/main/java/com/codewithProject/employee/service/AuthService.java
@@ -8,19 +8,25 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.ZonedDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 public class AuthService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
+    private final EmailService emailService;
 
     public AuthService(UserRepository userRepository,
-            PasswordEncoder passwordEncoder,
-            AuthenticationManager authenticationManager) {
+                       PasswordEncoder passwordEncoder,
+                       AuthenticationManager authenticationManager, EmailService emailService) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.authenticationManager = authenticationManager;
+        this.emailService = emailService;
     }
 
     public User register(String email, String password, String name) {
@@ -32,12 +38,41 @@ public class AuthService {
         user.setEmail(email);
         user.setPassword(passwordEncoder.encode(password));
         user.setName(name);
+        user.setEnabled(false);
 
-        return userRepository.save(user);
+        String token = UUID.randomUUID().toString();
+        user.setVerificationToken(token);
+        user.setTokenExpiration(ZonedDateTime.now().plusHours(24));
+
+         userRepository.save(user);
+
+        emailService.sendVerificationEmail(user.getEmail(), token);
+        return user;
     }
 
     public Authentication authenticate(String email, String password) {
+
+        var user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Utilisateur non trouvé"));
+
+        if (!user.isEnabled()) {
+            throw new RuntimeException("Veuillez d'abord vérifier votre adress e-mail avant de vous connecter.");
+        }
+
         return authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(email, password));
+    }
+
+    public Optional<User> verifyUser(String token) {
+        return userRepository.findByVerificationToken(token).map(user -> {
+            if (user.getTokenExpiration().isBefore(ZonedDateTime.now())) {
+                return null;
+            }
+            user.setEnabled(true);
+            user.setVerificationToken(null);
+            user.setTokenExpiration(null);
+            userRepository.save(user);
+            return user;
+        });
     }
 }
