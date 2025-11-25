61c787da9fdbccb69f179b1f1c7a63b5ba9312ff
unknown <louizadjoudad@gmail.com>
2025-11-25 16:20:08 +0100

    feat: add LoginSuccessResponse structure

--- src/main/java/com/codewithProject/employee/response/LoginSuccessResponse.java
+++ src/main/java/com/codewithProject/employee/response/LoginSuccessResponse.java
@@ -1,4 +1,21 @@
 package com.codewithProject.employee.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Schema(description = "Réponse apres connexion réussie")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class LoginSuccessResponse {
+
+    @Schema(description = "Message de succes", example = "Succes")
+    private String message;
+
+    @Schema(description = "access Token", example ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..." )
+    private String accessToken;
 }
