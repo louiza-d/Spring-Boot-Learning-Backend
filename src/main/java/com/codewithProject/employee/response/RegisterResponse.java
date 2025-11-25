751fc1b585aff60f92871adf4ae67e30c616f347
unknown <louizadjoudad@gmail.com>
2025-11-25 16:21:37 +0100

    feat: add LoginSuccessResponse

--- src/main/java/com/codewithProject/employee/response/RegisterResponse.java
+++ src/main/java/com/codewithProject/employee/response/RegisterResponse.java
@@ -1,4 +1,22 @@
 package com.codewithProject.employee.response;
 
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Schema(description = "Reponse apres inscription")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class RegisterResponse {
+
+    @Schema(description = "Message de confirmation", example = "Un email de verification vous a été envoyé")
+    private String message;
+
+    @Schema(description = "Nom complet de l'utilisateur", example = "LOUIZ Louiza")
+    private String name;
 }
