2ee7efc1fe9ef0822c05671083217f45d8dfe5ed
unknown <louizadjoudad@gmail.com>
2025-11-26 15:15:40 +0100

    feat(openapi): add OpenApiConfig with HTTP bearer security scheme (bearerAuth) for Swagger UI

---
+++ src/main/java/com/codewithProject/employee/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package com.codewithProject.employee.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+    }
+}
