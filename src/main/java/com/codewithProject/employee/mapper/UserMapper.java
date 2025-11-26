f93c6dff84e793a734fd7fc4bce26ec456c474d3
unknown <louizadjoudad@gmail.com>
2025-11-26 15:23:06 +0100

    chore(mapper): remove UserMapper content

--- src/main/java/com/codewithProject/employee/mapper/UserMapper.java
+++ src/main/java/com/codewithProject/employee/mapper/UserMapper.java
@@ -1,11 +1,9 @@
 package com.codewithProject.employee.mapper;
 
-import com.codewithProject.employee.entity.User;
-import com.codewithProject.employee.response.AuthResponse;
+
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-    AuthResponse.UserInfo toUserInfo(User user);
+ 
 }
