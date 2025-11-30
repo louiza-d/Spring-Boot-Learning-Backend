695d0345fa78deb24499e8412e354e62f20a3606
unknown <louizadjoudad@gmail.com>
2025-11-30 16:13:03 +0100

    Config: register MDCFilter as servlet filter

--- src/main/java/com/codewithProject/employee/config/WebConfig.java
+++ src/main/java/com/codewithProject/employee/config/WebConfig.java
@@ -3,6 +3,9 @@ package com.codewithProject.employee.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import com.codewithProject.employee.filter.MDCFilter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -15,5 +18,14 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
+    }
+
+    @Bean
+    public FilterRegistrationBean<MDCFilter> mdcFilterRegistration() {
+        FilterRegistrationBean<MDCFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new MDCFilter());
+        registration.addUrlPatterns("/*");
+        registration.setOrder(1);
+        return registration;
     }
 }
